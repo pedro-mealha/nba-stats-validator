ca76f17f84b94590e4aa56ba309428699bae23c0
WeNeedThePoh <pedromealha@hotmail.com>
2021-03-18 22:57:38 +0000

    add quarters score

--- assets/js/populate.js
+++ assets/js/populate.js
@@ -10,6 +10,15 @@ export async function populateHtml (data) {
   document.getElementById('host-team-points').innerHTML = data.teams.host.score.final
   document.getElementById('visitor-team-points').innerHTML = data.teams.visitor.score.final
 
+  document.getElementById('host-points-1p').innerHTML = data.teams.host.score.firstP
+  document.getElementById('visitor-points-1p').innerHTML = data.teams.visitor.score.firstP
+  document.getElementById('host-points-2p').innerHTML = data.teams.host.score.secondP
+  document.getElementById('visitor-points-2p').innerHTML = data.teams.visitor.score.secondP
+  document.getElementById('host-points-3p').innerHTML = data.teams.host.score.thirdP
+  document.getElementById('visitor-points-3p').innerHTML = data.teams.visitor.score.thirdP
+  document.getElementById('host-points-4p').innerHTML = data.teams.host.score.fourthP
+  document.getElementById('visitor-points-4p').innerHTML = data.teams.visitor.score.fourthP
+
   document.getElementById('open-file-btn').style.display = 'none'
   document.getElementById('content').style.display = 'block'
 }
