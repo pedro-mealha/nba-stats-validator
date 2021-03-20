866a35ab74304888215e1992faa357612af1c2f1
WeNeedThePoh <pedromealha@hotmail.com>
2021-03-20 22:22:29 +0000

    finish deployment process

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 const gulp = require('gulp')
 const uglify = require('gulp-uglify')
-const rename = require('gulp-rename')
 const clean = require('gulp-clean')
 const browserSync = require('browser-sync')
 
 gulp.task('build', function () {
   return gulp.src('./assets/js/*.js')
     .pipe(clean(), { force: true })
-    .pipe(rename({ suffix: '.min' }))
     .pipe(uglify())
     .pipe(gulp.dest('build'))
 })
 
 gulp.task('build-dev', function () {
   return gulp.src('./assets/js/*.js')
-    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('build'))
 })
 
