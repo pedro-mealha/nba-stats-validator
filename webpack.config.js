91bb3eccac49134c9b5cfc9fdec87a46dfa93151
WeNeedThePoh <pedromealha@hotmail.com>
2021-03-20 23:46:03 +0000

    fix css file path

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   entry: './assets/js/index.js',
   mode: 'development',
   output: {
-    path: `${__dirname}/public/dist`,
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: 'style-loader',
+            options: { 
+              insert: 'head', // insert style tag inside of <head>
+              injectType: 'singletonStyleTag' // this is for wrap all your style in just one style tag
+            },
+          },
+          "css-loader",
+          "sass-loader"
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./assets/css/index.css", to: "index.css" },
+      ],
+    }),
+  ]
 };
