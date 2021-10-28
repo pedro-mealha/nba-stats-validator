e9ab91540243d1f34533f75148e3c84731db0fe1
WeNeedThePoh <pedromealha@hotmail.com>
2021-10-28 22:46:14 +0100

    add pull request action

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path')
 
 module.exports = {
   entry: './assets/js/index.js',
@@ -18,23 +18,23 @@ module.exports = {
         use: [
           {
             loader: 'style-loader',
-            options: { 
+            options: {
               insert: 'head', // insert style tag inside of <head>
               injectType: 'singletonStyleTag' // this is for wrap all your style in just one style tag
-            },
+            }
           },
-          "css-loader",
-          "sass-loader"
-        ],
-      },
-    ],
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
   },
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin(),
-      new CssMinimizerPlugin(),
-    ],
+      new CssMinimizerPlugin()
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -43,8 +43,8 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: "./assets/css/index.css", to: "index.css" },
-      ],
-    }),
+        { from: './assets/css/index.css', to: 'index.css' }
+      ]
+    })
   ]
 };
